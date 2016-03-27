1ccc0eddfb4eee4eabada80b804331f95d63c098
Steve Pentler <stevepentler@gmail.com>
2016-03-27 12:12:04 -0600

    Passes sad path tests for create

--- test/controllers/api/v1/ideas_controller_test.rb
+++ test/controllers/api/v1/ideas_controller_test.rb
@@ -52,4 +52,25 @@ class Api::V1::IdeasControllerTest < ActionController::TestCase
     assert_equal idea[:body], json_response["body"]
     assert_equal "swill", json_response["quality"]
   end
+
+  test '#create rejects ideas without a title' do
+    idea = {body: "Something"}
+    number_of_ideas = Idea.all.count
+
+    post :create, idea: idea, format: :json
+
+    assert_response 422
+    assert_includes json_response["errors"]["title"], "can't be blank" 
+  end
+
+  test '#create rejects ideas without a body' do
+    idea = {title: "New Title"}
+    number_of_ideas = Idea.all.count
+
+    post :create, idea: idea, format: :json
+
+    assert_response 422
+    assert_includes json_response["errors"]["body"], "can't be blank" 
+  end
+
 end
