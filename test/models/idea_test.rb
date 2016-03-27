6bc068ca9365093d14fc02cdadd1596e12e248d0
Steve Pentler <stevepentler@gmail.com>
2016-03-27 12:30:06 -0600

    Removes enum, adds inclusion validations for quality and model tests

--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -1,9 +1,28 @@
 require 'test_helper'
 
 class IdeaTest < ActiveSupport::TestCase
-  test 'should have a quality that defaults to 0' do 
-    idea = Idea.new
-    assert_equal("swill", idea.quality)
+  test 'should have a quality that defaults to swill' do 
+    ideas(:one).quality = "swill"
+
+    assert(ideas(:one).valid?)
+  end
+
+  test 'it is valid with a quality of plausible' do 
+    ideas(:one).quality = "plausible"
+
+    assert(ideas(:one).valid?)
+  end
+
+  test 'it is valid with a quality of genius' do 
+    ideas(:one).quality = "genius"
+
+    assert(ideas(:one).valid?)
+  end
+
+  test 'it is invalid with any other quality' do 
+    ideas(:one).quality = "invalid"
+
+    refute(ideas(:one).valid?)
   end
 
   test 'it should be invalid without a title or body' do 
