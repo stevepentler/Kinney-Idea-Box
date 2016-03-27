debbb6a4a63b0af5b5c450a90c27e12ef94bed12
Steve Pentler <stevepentler@gmail.com>
2016-03-27 12:09:31 -0600

    Passes happy path tests for create

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -4,5 +4,24 @@ class Api::V1::IdeasController < ApplicationController
   def index
     respond_with Idea.all
   end
-  
+
+  def show
+    respond_with Idea.find(params[:id])
+  end
+
+  def create
+    idea = Idea.new(idea_params)
+    if idea.save
+      respond_with(idea, status:201, location: api_v1_idea_path(idea))
+    else
+      render json: {errors: idea.errors}, status: 422, location: api_v1_ideas_path
+    end
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:body, :title)
+  end
+
 end
--- test/controllers/api/v1/ideas_controller_test.rb
+++ test/controllers/api/v1/ideas_controller_test.rb
@@ -27,4 +27,29 @@ class Api::V1::IdeasControllerTest < ActionController::TestCase
       assert idea["quality"]
     end
   end
+
+  test 'show action responds to json' do 
+    id = ideas(:one).id
+
+    get :show, id: id, format: :json
+    assert_equal id, json_response["id"]
+  end
+
+  test '#create adds an additional idea to the database' do 
+    assert_difference 'Idea.count', 1 do 
+      idea = {title: "New Idea", body: "Something"}
+
+      post :create, idea: idea, format: :json
+    end
+  end
+
+  test '#create returns the new idea' do 
+    idea = {title: "New Idea", body: "Something"}
+
+    post :create, idea: idea, format: :json 
+
+    assert_equal idea[:title], json_response["title"]
+    assert_equal idea[:body], json_response["body"]
+    assert_equal "swill", json_response["quality"]
+  end
 end
